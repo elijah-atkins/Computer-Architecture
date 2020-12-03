88544643ca5e269cabb94f9435213c0c3830c16d
Elijah-Atkins <elijahorama@gmail.com>
2020-12-02 23:20:41 -0700

    adding actions

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,11 +2,18 @@
 
 import sys
 
-# Program Actions
-LDI = 130 # LOAD IMMEDIATE
-PRN = 71 # PRINT
-HLT = 1 # HALT
-MUL = 162 #MULTIPLY
+# Program Actions using hex for easy ref in trace
+LDI = 0x82 # 130 LOAD IMMEDIATE
+PRN = 0x47 # 71 PRINT
+HLT = 0x01 # 1 HALT
+MUL = 0xA2 # 162 MULTIPLY
+ADD = 0xA0 # 160 ADD
+AND = 0xA8 # 168 AND
+CALL = 0x50 # 80 CALL
+CMP = 0xA7 # 167 Compare the values in two registers.
+DEC = 0x66 # 102 Decrement (subtract 1 from) the value in the given register.
+DIV = 0xA3 # Divide the value in the first register by the value in the second, storing the result in registerA.
+INC = 0x65 # Increment (add 1 to) the value in the given register.
 
 
 class CPU:
@@ -16,11 +23,11 @@ class CPU:
         """Construct a new CPU."""
         self.ram = [0] * 256 # 256 bytes of memory
         self.reg = [0] * 7 # General Purpose Registers R0 - R6
-        self.reg.append('0xF4') # R7 set to '0xF4'
+        self.reg.append(244) # R7 set to '0xF4'
         self.pc = 0 # Program Counter
         self.fl = 0
         self.ie = 0
-
+    # TODO implament branchtable https://en.wikipedia.org/wiki/Branch_table
     # access the RAM inside the CPU object
     # MAR (Memory Address Register) - contains the address that is 
         # being read / written to
@@ -38,39 +45,30 @@ class CPU:
     def load(self):
         """Load a program into memory."""
 
-        address = 0
+        if len(sys.argv) != 2:
+            print("Usage: ls8.py filename")
+            sys.exit(1)
 
-        # For now, we've just hardcoded a program:
+        try:
+            address = 0
+            with open(sys.argv[1]) as ls8:
+                for line in ls8:
+                    split_line = line.split('#') #remove pounds from code
+                    code_value = split_line[0].strip() #recover binary
+                    if code_value == '': #handle empty lines
+                        continue
+                    try:
+                        code_value = int(code_value, 2)
+                    except ValueError:
+                        print(f"Invalid Number: {code_value}")
+                        sys.exit(1)
 
-        program = [
-            130,0,8,71,0,1 #print8.ls8 in decimal 
-            # From print8.ls8
-            # 0b10000010, # LDI R0,8
-            # 0b00000000,
-            # 0b00001000,
-            # 0b01000111, # PRN R0
-            # 0b00000000,
-            # 0b00000001, # HLT
-            #from mult.ls8
-            # 0b10000010, # LDI R0,8
-            # 0b00000000,
-            # 0b00001000,
-            # 0b10000010, # LDI R1,9
-            # 0b00000001,
-            # 0b00001001,
-            # 0b10100010, # MUL R0,R1
-            # 0b00000000,
-            # 0b00000001,
-            # 0b01000111, # PRN R0
-            # 0b00000000,
-            # 0b00000001, # HLT
+                    self.ram_write(code_value, address)
+                    address += 1
 
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+        except FileNotFoundError:
+            print(f"{sys.argv[1]} file not found")
+            sys.exit(2)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -87,13 +85,21 @@ class CPU:
         from run() if you need help debugging.
         """
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
+        print(f"TRACE: %02X %02X %02X | %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X |" % (
             self.pc,
             self.fl,
             self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
+            self.ram_read(self.pc + 2),
+            self.ram_read(self.pc + 3),
+            self.ram_read(self.pc + 4),
+            self.ram_read(self.pc + 5),
+            self.ram_read(self.pc + 6),
+            self.ram_read(self.pc + 7),
+            self.ram_read(self.pc + 8),
+            self.ram_read(self.pc + 9),
+            self.ram_read(self.pc + 10),
         ), end='')
 
         for i in range(8):
@@ -112,9 +118,9 @@ class CPU:
             IR = self.ram_read(self.pc)
 
             # ram_read() - read bytes at PC + 1 and PC + 2 from RAM into variables 
-            # register_a and register_b
-            register_a = self.ram_read(self.pc + 1)
-            register_b = self.ram_read(self.pc + 2)
+            # operand_a and operand_b
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
 
             # HALT
             if IR == HLT:
@@ -126,28 +132,21 @@ class CPU:
             # PRINT
             elif IR == PRN:
                 # Print Reg
-                print(self.reg[register_a])
+                print(self.reg[operand_a])
                 # Update PC
                 self.pc += 2
 
             # LDI = LOAD IMMEDIATE
             elif IR == LDI:
                 # Assign value to Reg Key
-                self.reg[register_a] = register_b
+                self.reg[operand_a] = operand_b
                 # Update PC
                 self.pc += 3
             #MUL = Multiply
             elif IR == MUL:
                 # Get product
-                product_of_register = self.reg[register_a] * self.reg[register_b]
+                product_of_operand = self.reg[operand_a] * self.reg[operand_b]
                 # Assign value to Reg Key
-                self.reg[register_a] = product_of_register
+                self.reg[operand_a] = product_of_operand
                 # Update PC
                 self.pc += 3
-
-    # elif command_to_execute == ADD:
-    #     register_a = memory[program_counter +1]
-    #     register_b = memory[program_counter +2]
-    #     sum_of_registers = registers[register_a] + registers[register_b]
-    #     registers[register_a] = sum_of_registers
-    #     program_counter += 3
